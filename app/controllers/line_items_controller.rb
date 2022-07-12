d1c597157ab8568de80e8b9b4f524d9974c888de
Henry Teng <heteng@umich.edu>
2022-07-12 15:08:09 -0400

    successfully working cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -64,11 +64,11 @@ class LineItemsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
-      @line_item = LineItem.find(params[:id, :cart_id])
+      @line_item = LineItem.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
